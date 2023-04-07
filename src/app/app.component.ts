4d97905e26e6f6b13935cca8453874f9d2a6030e
michielkikkert <michiel@kikkert.nl>
2023-04-07 19:44:42 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,84 @@
 import {
-    ChangeDetectionStrategy,
-    Component,
-    OnInit,
-    SettableSignal,
-    signal
+	ChangeDetectionStrategy,
+	Component,
+	OnInit,
+	SettableSignal,
+	signal,
+	effect,
+	computed
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 
 type signalRow = SettableSignal<{ value: string, count: number }>
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [CommonModule],
-    changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	standalone: true,
+	imports: [CommonModule],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-    public title: SettableSignal<string> = signal('');
-    public table: SettableSignal<signalRow[]> = signal([
-        signal({value: 'row1', count: 0}),
-        signal({value: 'row2', count: 0}),
-        signal({value: 'row3', count: 0}),
-        signal({value: 'row4', count: 0}),
-        signal({value: 'row5', count: 0}),
-    ])
+	public title: SettableSignal<string> = signal('Hallo');
+	public name: SettableSignal<string> = signal('Michiel');
 
-    ngOnInit() {
-       this.title.set('title set')
-;    }
+	public combi = computed(() => {
+		return this.title() + ' ' + this.name();
+	})
 
-    randomUpdate() {
-        this.table.update( (rows) => {
-            const random = Math.floor(Math.random() * rows.length);
-            rows[random].mutate((row) => ({...row, count: row.count++})) ;
-            return rows;
-        });
-    }
+	public table: SettableSignal<signalRow[]> = signal([
+		signal({value: 'row1', count: 0}),
+		signal({value: 'row2', count: 0}),
+		signal({value: 'row3', count: 0}),
+		signal({value: 'row4', count: 0}),
+		signal({value: 'row5', count: 0}),
+	])
 
-    addRow() {
-        this.table.mutate((rows) => {
-            rows.push(signal({value: 'added', count: 0}));
-        });
-    }
+	ngOnInit() {
+		// this.title.set('title set');
 
-    incrementRow(row: signalRow):void {
-        row.mutate( row => {
-            row.count++
-        })
-    }
+		effect(() => {
+			console.log('Why do I trigger..?')
+		});
+
+		effect(() => {
+			this.table()
+			console.log('Table Updated!')
+		});
+
+		effect(() => {
+			this.table().forEach(row => row()); // This works nicely!
+			console.log('A row updated!')
+		});
+
+		effect(() => {
+			const row = this.table()[0]();
+			console.log('Row 0 updated to', row.count)
+		});
+	}
+
+	randomUpdate() {
+		const random = Math.floor(Math.random() * this.table().length);
+		this.table()[random].mutate((row) => ({...row, count: row.count++}));
+	}
+
+	addRowMutate() {
+		this.table.mutate((rows) => {
+			rows.push(signal({value: 'added', count: 0}));
+		});
+	}
+
+	addRowUpdate() {
+		this.table.update((rows) => {
+			return [...rows, signal({value: 'added', count: 0})]
+		});
+	}
+
+	incrementRow(row: signalRow): void {
+		row.mutate(row => {
+			row.count++
+		})
+	}
 
 }
